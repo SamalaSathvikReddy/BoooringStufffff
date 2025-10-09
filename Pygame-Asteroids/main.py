9d5bcf61ef1ba8cb47fe54c691067504f0eba063
SamalaSathvikReddy <samalasathvikreddy7002@gmail.com>
2025-10-09 21:28:19 +0530

    Added Player objects

--- Pygame-Asteroids/main.py
+++ Pygame-Asteroids/main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import * 
+from player import Player
+
 def main():
     pygame.init()
     print("Starting-Asteroids!")
@@ -8,11 +10,23 @@ def main():
     print(SCREEN_WIDTH)
     """
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    start_clock = pygame.time.Clock()
+    dt = 0
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        x = SCREEN_WIDTH/2
+        y = SCREEN_HEIGHT/2
+        player = Player(x, y)
+        player.draw(screen)
         pygame.display.flip()         
+         
+        # Limits the fps till 60
+        dt = (start_clock.tick(60))/1000
+        
+
 if __name__ == "__main__":
     main()
