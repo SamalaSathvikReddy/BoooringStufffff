1762bc573fd7df60c3bd3e9ae341978fcf01023c
SamalaSathvikReddy <samalasathvikreddy7002@gmail.com>
2025-10-10 22:08:41 +0530

    Created Asteroid Objects visible in the game

--- Pygame-Asteroids/main.py
+++ Pygame-Asteroids/main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import * 
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -15,16 +17,32 @@ def main():
    
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
+
+
+    
+    # Creating Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable) 
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+
     player = Player(x, y)
- 
+    asteroid_field = AsteroidField() 
+    # Game Loop 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)  
+        
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()         
          
         # Limits the fps till 60
